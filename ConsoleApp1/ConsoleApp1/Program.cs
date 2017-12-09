f7762e47a55b4e27b6c79810f7f5de77c6fb2c9f
Box-Bop <Box-Bop@github.com>
2017-12-09 14:06:51 +0200

    Ex 1 done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int i, num1, num2;
+            Console.WriteLine("sisestage esimene number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sisestage teine number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("paarisarvud nende kahe arvu vahel on: ");
+            if (num1 % 2 != 0)
+            {
+                num1++;
+            }
+            for (i = num1; i <= num2; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
         }
     }
 }
