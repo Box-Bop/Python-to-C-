872726a1dd4cd66f3dcd4591dd04cd5fa897c9d8
Box-Bop <Box-Bop@github.com>
2017-12-09 14:17:29 +0200

    ex 2 backup

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader(fileName);
+
+            int counter = 0;
+            int five = 0;
+            string delim = " ";
+            string[] fields = null;
+            string line = null;
+
+            while (!sr.EndOfStream)
+            {
+                counter += fields.Length;
+            }
+            Console.WriteLine("Failis " + fileName + " on kokku: {0} sõnu.", counter);
+
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+
+            string[] words = File.ReadAllText(path).Split(' ');
+            foreach (string word in words)
+            {
+                if (word.Length <= 4)
+                {
+                    five += 1;
+                }
+
+            }
+            Console.WriteLine("Nendest {0} on väiksemad kui 5 tähte.",five);
+
+            Console.ReadLine();
         }
     }
 }
