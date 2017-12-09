7ac771aec0073a91b3c98cb4be6c73f7ef452035
Box-Bop <Box-Bop@github.com>
2017-12-09 17:45:35 +0200

    Merge branch 'master' into h-3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -52,13 +52,14 @@ namespace ConsoleApp1
             int[] nums1 = new int[] { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
             int[] nums2 = new int[] { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7 };
             Console.WriteLine("Sarnased elemendid mõlemast list'ist");
-            foreach (int num1 in nums1)
+
+            foreach (int numb1 in nums1)
             {
-                foreach (int num2 in nums2)
+                foreach (int numb2 in nums2)
                 {
-                    if (num1 == num2)
+                    if (numb1 == numb2)
                     {
-                        Console.WriteLine(num1);
+                        Console.WriteLine(numb1);
                     }
                 }
             }
