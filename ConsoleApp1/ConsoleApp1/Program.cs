e7f5e9246b05a919625d1fdcc8d7ff64d1b372a8
Box-Bop <Box-Bop@github.com>
2017-12-09 18:00:15 +0200

    Finishing touches (final commit)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -25,7 +25,7 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(i);
             }
-
+            Console.WriteLine("\n\n");
 
             string filePath = Directory.GetCurrentDirectory();
             Console.WriteLine("Sisestage fail'i nimi (kttekst.txt) : ");
@@ -47,7 +47,7 @@ namespace ConsoleApp1
 
             Console.WriteLine("Failis " + fileName + " on kokku: {0} sõnu.", counter);
             Console.WriteLine("Nendest {0} on väiksemad kui 5 tähte.",five);
-
+            Console.WriteLine("\n\n");
             int[] nums1 = new int[] { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
             int[] nums2 = new int[] { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7 };
             Console.WriteLine("Sarnased elemendid mõlemast list'ist");
@@ -71,9 +71,12 @@ namespace ConsoleApp1
             Console.WriteLine("Esimese list'i keskmine on: " + avg1);
             Console.WriteLine("Teise list'i keskmine on: " + avg2);
             Console.WriteLine("Mõlema list'ide keskmine on: " + avg3);
-
+            Console.WriteLine("\n");
             Console.WriteLine("Esimese list'i suurim number: " + nums1.Max());
+            Console.WriteLine("Esimese list'i väikseim number: " + nums1.Min());
+            Console.WriteLine("\n");
             Console.WriteLine("Teise list'i suurim number: " + nums2.Max());
+            Console.WriteLine("Teise list'i väikseim number: " + nums2.Min());
 
             Console.ReadLine();
         }
